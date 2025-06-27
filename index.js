bbb23598c5fb67da2a60de6d9fff8d59452360f7
Jacob Gardner <juhucub@gmail.com>
2025-06-27 16:05:49 -0700

    Receiving remote redux actions

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ import startServer from "./src/server";
 
 export const store = makeStore();
 /*SIncr we also export the store, you can fire up a node REPL and import the store to play with it interactively. e.g babel-node */
-startServer();
+startServer(store);
+
+// Set initial entries and trigger the first vote
+store.dispatch({
+    type: 'SET_ENTRIES',
+    entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,27 @@
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 
-export default function startServer() {
-    const io = new Server(4000);
+
+/* How the server works
+*1. Cient sends an action to the server
+*2. Server hands action to Redux store
+*3. The store calls the reducer and the reducer executes the logic related to the action
+*4. The store updates its state based on the return value of the reducer.
+*5. The store executes the listener function subsribed by the server
+*6. The server emits a 'state' event
+*7. All conected clients - including the ones that initiated the original action - receive the new state
+*/
+export default function startServer(store) {
+    const io = new Server().attach(8090);
+
+    store.subscribe(
+        () => io.emit('state', store.getState().toJS())
+    );
+
+    //listen to 'connection' events on server. When a client connects, we send the current state to the client immediately - very useful
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+        //Receiving remote Reduxe actions //FIXME: LOLOLOL HUGEST MEGA SECURITY :( any connected socket.io hacker can send any action to the store, so we should never use this)
+        //Should be some kind of firewall here, one similar to the one in the client, which only allows certain actions to be sent to the store
+        socket.on('action', store.dispatch.bind(store));
+    });
 }
